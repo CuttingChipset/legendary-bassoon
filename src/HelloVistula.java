44549ca05e493d9a5509640a2ea92c4439939d2e
Mikołaj Jaskulski <miki.jaskolka@gmail.com>
2024-10-12 14:22:02 +0200

    Test breakpoints

--- src/HelloVistula.java
+++ src/HelloVistula.java
@@ -1,6 +1,8 @@
 public class HelloVistula {
 
     public static void main(String[] args) {
-        System.out.println("Hello Vistula");
+        int numba = 68524;
+        String numba2 = String.valueOf(numba);
+        System.out.println("Hello Vistula, this is " + numba2);
     }
 }
